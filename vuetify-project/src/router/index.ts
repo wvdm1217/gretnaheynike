6fabdd3f0f2df3c53341a8f389b07dd50555e40e
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-24 16:00:55 +0200

    feat: redirect

--- vuetify-project/src/router/index.ts
+++ vuetify-project/src/router/index.ts
@@ -10,6 +10,11 @@ const routes = [
         path: '',
         name: 'Home',
         component: () => import('@/views/Home.vue'),
+      },
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('@/views/About.vue'),
       }
     ],
   },
