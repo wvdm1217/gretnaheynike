81592dc11f4e3de517701a0bbb92bb86ba07bba4
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-24 14:23:25 +0200

    feat: home page layout

---
+++ vuetify-project/src/components/HomePage.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+
+
+      <h1 class="text-h1 font-weight-bold">GRETNA HEYNIKE</h1>
+
+      <div class="text-body-6 font-weight-bold mb-n1 mt-1">Violinist | Musical Educator | Professional Musician</div>
+
+      <div class="py-14" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-btn
+            href="/about"
+            min-width="164"
+            rel="noopener noreferrer"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-music-note-eighth"
+              size="large"
+              start
+            />
+
+            About
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            href="/teaching"
+            min-width="164"
+            rel="noopener noreferrer"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-music-note-quarter"
+              size="large"
+              start
+            />
+
+            Teaching
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            href="/performance"
+            min-width="164"
+            rel="noopener noreferrer"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-music-note-half"
+              size="large"
+              start
+            />
+
+            Performance
+          </v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            href="/contact"
+            min-width="164"
+            rel="noopener noreferrer"
+            variant="text"
+          >
+            <v-icon
+              icon="mdi-music-note"
+              size="large"
+              start
+            />
+
+            Contact
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
--- vuetify-project/src/views/Home.vue
+++ vuetify-project/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <HelloWorld />
+  <HomePage />
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import HomePage from '@/components/HomePage.vue';
 </script>
