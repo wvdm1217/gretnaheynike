1941b776a1df90e6a6082b2e48b8b4cb5331d720
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-24 16:09:43 +0200

    revert: use previous setup

--- vuetify-project/src/router/index.ts
+++ vuetify-project/src/router/index.ts
@@ -1,23 +1,27 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
 
 const routes = [
   {
     path: '/',
-    component: Home,
-    name: 'home',
-  },
-  {
-    path: '/about',
-    component: About,
-    name: 'about',
-  },
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('@/views/Home.vue'),
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('@/views/About.vue'),
+      },
+    ],
+  }
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 })
 
