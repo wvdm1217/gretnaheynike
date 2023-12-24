00c8af63583441da60b80aeb945bb5a8aa536669
Werner van der Merwe <wvdm1217@gmail.com>
2023-12-24 15:41:27 +0200

    fix: remove child path

--- vuetify-project/src/router/index.ts
+++ vuetify-project/src/router/index.ts
@@ -10,12 +10,7 @@ const routes = [
         path: '',
         name: 'Home',
         component: () => import('@/views/Home.vue'),
-      },
-      {
-        path: '/about',
-        name: 'About',
-        component: () => import('@/views/About.vue'),
-      },
+      }
     ],
   },
   {
